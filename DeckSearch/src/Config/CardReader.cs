f5212cd294fdc94f49d94b5f64b9e46db01061f5
Yulun Zhang <yulunzha@usc.edu>
2020-02-07 19:08:20 -0800

    removed CardDefs.xml; changed basicWarlock.tml back

--- DeckSearch/src/Config/CardReader.cs
+++ DeckSearch/src/Config/CardReader.cs
@@ -8,84 +8,71 @@ using SabberStoneCore.Model;
 
 namespace DeckSearch.Config
 {
-   class CardReader
-   {
-      public static CardClass GetClassFromName(string className)
-      {
-         className = className.ToUpper();
-         foreach (CardClass curClass in Cards.HeroClasses)
-            if (curClass.ToString().Equals(className))
-               return curClass;
+    class CardReader
+    {
+        public static CardClass GetClassFromName(string className)
+        {
+            className = className.ToUpper();
+            foreach (CardClass curClass in Cards.HeroClasses)
+                if (curClass.ToString().Equals(className))
+                    return curClass;
 
-         Console.WriteLine("Card class "+className
-                           +" not a valid hero class.");
-         return CardClass.NEUTRAL;
-      }
+            Console.WriteLine("Card class " + className
+                              + " not a valid hero class.");
+            return CardClass.NEUTRAL;
+        }
 
-      public static CardSet[] GetSetsFromNames(string[] cardSets)
-      {
-         Console.WriteLine(String.Join(" ", cardSets));
-         var sets = new CardSet[cardSets.Length];
+        public static CardSet[] GetSetsFromNames(string[] cardSets)
+        {
+            Console.WriteLine(String.Join(" ", cardSets));
+            var sets = new CardSet[cardSets.Length];
 
-         for (int i=0; i<cardSets.Length; i++)
-            foreach (CardSet c in Enum.GetValues(typeof(CardSet)))
-               if (c.ToString().Equals(cardSets[i]))
-                  sets[i] = c;
+            for (int i = 0; i < cardSets.Length; i++)
+                foreach (CardSet c in Enum.GetValues(typeof(CardSet)))
+                    if (c.ToString().Equals(cardSets[i]))
+                        sets[i] = c;
 
-         return sets;
-      }
+            return sets;
+        }
 
-		public static List<Card> GetCards(CardClass heroClass, CardSet[] sets)
-      {
-         var hs = new HashSet<Card>();
-         var cards = new List<Card>();
-         List<Card> allCards = GetAllCards();
-         foreach (Card c in allCards)
-         {
-            if (c != Cards.FromName("Default")
-                && c.Implemented
-                && c.Collectible
-                && c.Type != CardType.HERO
-                && c.Type != CardType.ENCHANTMENT
-                && c.Type != CardType.INVALID
-                && c.Type != CardType.HERO_POWER
-                && c.Type != CardType.TOKEN
-                && (c.Class == CardClass.NEUTRAL || c.Class == heroClass)
-                && !hs.Contains(c))
-            {
-					bool validSet = false;
-               foreach (CardSet curSet in sets)
-						validSet |= (c.Set == curSet);
 
-               if (validSet)
-               {
-                  cards.Add(c);
-                  hs.Add(c);
-               }
+        private static void printCards(List<Card> cards) {
+            Console.WriteLine("Card set size = " + cards.Count);
+            foreach (Card c in cards) {
+                Console.WriteLine(c.Name);
             }
-         }
+        }
 
-         return cards;
-      }
- 
-		public static List<Card> GetAllCards()
-      {
-         string fileName = "resources/CardDefs.xml";
-         XDocument doc = XDocument.Load(fileName);
-         var authors = doc.Descendants("Entity")
-                        .Descendants("Tag")
-                        .Where(x => x.Attribute("name").Value == "CARDNAME")
-                        .Elements("enUS");
-
-         var allCards = new List<Card>();
-         foreach(var c in authors)
-         {
-            if (Cards.FromName(c.Value) != null)
+        public static List<Card> GetCards(CardClass heroClass, CardSet[] sets)
+        {
+            var hs = new HashSet<Card>();
+            var cards = new List<Card>();
+            List<Card> allCards = Cards.All.ToList();
+            foreach (Card c in allCards)
             {
-               allCards.Add(Cards.FromName(c.Value));
+                if (c != Cards.FromName("Default")
+                    && c.Implemented
+                    && c.Collectible
+                    && c.Type != CardType.HERO
+                    && c.Type != CardType.ENCHANTMENT
+                    && c.Type != CardType.INVALID
+                    && c.Type != CardType.HERO_POWER
+                    && c.Type != CardType.TOKEN
+                    && (c.Class == CardClass.NEUTRAL || c.Class == heroClass)
+                    && !hs.Contains(c))
+                {
+                    bool validSet = false;
+                    foreach (CardSet curSet in sets)
+                        validSet |= (c.Set == curSet);
+
+                    if (validSet)
+                    {
+                        cards.Add(c);
+                        hs.Add(c);
+                    }
+                }
             }
-         }
-         return allCards;
-      }
-	}
+            return cards;
+        }
+    }
 }
